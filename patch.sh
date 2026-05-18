21bb8bc2e0856ce4dec3e89772c1d1eaa30865f5
koboshchan <lucaszhang1001@gmail.com>
2026-05-18 04:59:39 -0700

    patches and sctiprs

--- patch.sh
+++ patch.sh
@@ -1,1 +1,2 @@
 ./gradlew applyAllPatches
+./gradlew applyFoliaPatches
---
+++ rb.sh
@@ -0,0 +1,6 @@
+./gradlew rebuildFoliaPatches
+./gradlew rebuildFoliaServerPatches
+./gradlew rebuildPaperPatches
+./gradlew rebuildPaperServerPatches
+./gradlew rebuildServerPatches
+./gradlew rebuildMinecraftPatches
